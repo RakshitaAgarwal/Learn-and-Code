6cc12cefd6171044c7b808ee17c375ce699f5e26
rakshita.agarwal <rakshita.agarwal@intimetec.com>
2024-03-21 13:53:47 +0530

    improvised code

--- RemoveGetterSetter/ImmutableObjects/Person.java
+++ RemoveGetterSetter/ImmutableObjects/Person.java
@@ -1,8 +1,8 @@
 package RemoveGetterSetter.ImmutableObjects;
 
 public class Person {
-    private final String name;
-    private final int age;
+    public final String name;
+    public final int age;
 
     public Person(String name, int age) {
         this.name = name;
