e461f7939076d5746627fe8066669693bb88ea89
rakshita.agarwal <rakshita.agarwal@intimetec.com>
2024-05-19 10:38:10 +0530

    initial commit

---
+++ DependencyInversionPrinciple/EmployeeService.java
@@ -0,0 +1,30 @@
+package DependencyInversionPrinciple;
+
+//Apply the Dependency Inversion Principle (DIP) by refactoring a set of classes
+// Note: Also mention why this code violating DIP
+
+public class EmployeeService {
+    private Database database = new Database();
+
+    public void saveEmployee(Employee employee) {
+        database.save(employee);
+    }
+}
+
+public class Database {
+    public void save(Employee employee) {
+        // Code to save employee to the database
+    }
+}
+
+public class Employee {
+    private String name;
+    private String address;
+
+    public Employee(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters and setters
+}
